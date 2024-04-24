7e8f62494d39cc9461a46f9d29138be3d5d6301a
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-04-24 03:39:06 +0000

    P

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Halaman Utama' });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
